70877d83a2b9482e5a28200c004ba60a7b795e66
Aldiran Santos <contact.aldiransantos@gmail.com>
2023-09-08 18:52:21 -0300

    feat: add getDate range in DataContext

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import DateInput from './DateInput';
+import { useData } from '../Context/DataContext';
 
 const DateRange = () => {
-	const [inicio, setInicio] = React.useState('');
-	const [final, setFinal] = React.useState('');
+	const { inicio, setInicio, final, setFinal } = useData();
 
 	return (
-		<form onSubmit={(e) => e.preventDefault()}>
+		<form className="box flex" onSubmit={(e) => e.preventDefault()}>
 			<DateInput
 				label="Início"
 				value={inicio}
--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -5,6 +5,10 @@ type InterfaceDataContext = {
 	data: InterfaceVendas[] | null;
 	loading: boolean;
 	error: string | null;
+	inicio: string;
+	setInicio: React.Dispatch<React.SetStateAction<string>>;
+	final: string;
+	setFinal: React.Dispatch<React.SetStateAction<string>>;
 };
 
 type InterfaceVendas = {
@@ -25,13 +29,27 @@ export const useData = () => {
 	return context;
 };
 
+function getDate(n: number) {
+	const date = new Date();
+	date.setDate(date.getDate() - n);
+	const dd = String(date.getDate()).padStart(2, '0');
+	const mm = String(date.getMonth() + 1).padStart(2, '0');
+	const yyyy = date.getFullYear();
+	return `${yyyy}-${mm}-${dd}`;
+}
+
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+	const [inicio, setInicio] = React.useState(getDate(30));
+	const [final, setFinal] = React.useState(getDate(0));
+
 	const { data, loading, error } = useFetch<InterfaceVendas[]>(
-		'https://data.origamid.dev/vendas',
+		`https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`,
 	);
 
 	return (
-		<DataContext.Provider value={{ data, loading, error }}>
+		<DataContext.Provider
+			value={{ data, loading, error, inicio, setInicio, final, setFinal }}
+		>
 			{children}
 		</DataContext.Provider>
 	);
