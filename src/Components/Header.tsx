3f0367c9415c585c2221edaa1458a93179947074
Aldiran Santos <contact.aldiransantos@gmail.com>
2023-09-11 22:54:37 -0300

    feat: add sidenav area

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useData } from '../Context/DataContext';
 import DateRange from './DateRange';
 import Months from './Months';
 
 const Header = () => {
+	const [title, useTitle] = React.useState('Resumo');
+
 	return (
 		<header className="mb">
-			<div className="mb">
+			<div className="daterange mb">
 				<DateRange />
+				<h1 className="box bg-3">{title}</h1>
 			</div>
 			<Months />
 		</header>
